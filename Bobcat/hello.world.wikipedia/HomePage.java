b7b965ed014d354e69fa6f50d9bd811286b4003a
Olwen Davies <olwend@gmail.com>
2017-09-29 16:36:26 +0100

    Add files via upload

---
+++ Bobcat/hello.world.wikipedia/HomePage.java
@@ -0,0 +1,30 @@
+package com.hello.world.wikipedia;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+import com.cognifide.qa.bb.qualifier.PageObject;
+import com.google.inject.Inject;
+
+@PageObject
+public class HomePage {
+
+	private static final String URL = "https://en.wikipedia.org";
+
+	@Inject
+	private WebDriver webDriver;
+
+	@FindBy(id = "p-search")
+	private SearchComponent searchComponent;
+
+	public SearchComponent getSearchComponent() {
+		return searchComponent;
+	}
+}
+
+	public HomePage open() {
+		webDriver.get(URL);
+		return this;
+	}
+
+}
